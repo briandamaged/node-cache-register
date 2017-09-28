2587becd51683726166d096d8c142cb6baa5e5c2
Brian Lauber <brian.lauber@33across.com>
2017-09-28 17:23:43 -0400

    Storage objects available in top-level module again
    Yeah -- really need to finalize the module layout

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 
+const storage = require('./storage');
+
+Object.assign(exports, storage);
+
 exports.CacheRegister = require('./cache-register');
 
 exports.Fulfiller = require('./fulfiller');
 exports.Debouncer = require('./debouncer');
 
-exports.storage = require('./storage');
+exports.storage = storage;
 exports.keyFuncs = require('./key-funcs');
