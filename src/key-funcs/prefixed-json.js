50a2eb10853646d2e96e43763dbf55a28db9db8b
Brian Lauber <brian.lauber@33across.com>
2018-02-17 12:44:59 -0500

    PrefixedJSON can accept a String as a param
    Rather than having to pass the full {prefix: "foo"}

--- src/key-funcs/prefixed-json.js
+++ src/key-funcs/prefixed-json.js
@@ -2,7 +2,17 @@
 const jsonifyArgs = require('./jsonify-args');
 const Prefixer = require('./prefixer');
 
-function PrefixedJSON({prefix} = {}) {
+function PrefixedJSON(options = {}) {
+  // This is mostly for backwards compatibility
+  // with the 0.x API.
+  const opts = (
+    typeof(options) === 'string'
+    ? {prefix: options}
+    : options
+  );
+
+  const prefix = opts.prefix;
+
   return Prefixer({
     prefix,
     keyFunc: jsonifyArgs,
